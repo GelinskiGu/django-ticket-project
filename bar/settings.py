3c374669ed1c31b687960259a22be9df21d04024
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-31 12:53:58 -0300

    Ajustando deploy no render

--- bar/settings.py
+++ bar/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 from django.contrib.messages import constants
 
@@ -85,13 +87,17 @@ WSGI_APPLICATION = 'bar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
